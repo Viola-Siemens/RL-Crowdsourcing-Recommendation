8ecf9da7084ce25272e98c721f9fe6eb88681ef4
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-07-07 19:25:40 +0800

    主函数与各种模型的TODO

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
 import argparse
 
+from model.impl.ActorCritic import ActorCritic
+from model.impl.PolicyBased import PPO
+from model.impl.ValueBased import QLearning
+
+algorithms = {}
+
+for model in [ActorCritic(), PPO(), QLearning()]:
+    algorithms[model.get_algorithm_name()] = model
+
 parser = argparse.ArgumentParser()
+parser.add_argument("--algo", type=str, choices=algorithms.keys(), help="Reinforcement Learning Algorithm")
+parser.add_argument("--epochs", type=int, help="Total Epoch of Training")
+
+# 训练数据直接解压在src/resources中，如"src/resources/project_list.csv"、"src/resources/entry/entry_19393_24.txt"等
 
 if __name__ == "__main__":
-    print("Hello World")
+    # TODO
+    parser.print_help()
--- src/model/impl/ActorCritic.py
+++ src/model/impl/ActorCritic.py
@@ -3,10 +3,15 @@ from typing import Callable
 from torch.optim import Optimizer
 
 from data.Environment import Environment
+from model.FCNet import FCNet
 from model.ReinforcementAlgorithm import ReinforcementAlgorithm
 
 
 class ActorCritic(ReinforcementAlgorithm):
+    def __init__(self):
+        net = FCNet(4, [(8, None)])  # TODO
+        super().__init__(net)
+
     def train(self, env: Environment, optimizer: Optimizer, epochs: int,
               logger: Callable[[int, float], None], **kwargs) -> None:
         # TODO
