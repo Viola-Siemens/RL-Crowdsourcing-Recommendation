023f5f2a2952015ec0535c8a2cb903163b429186
Qianyi Zhao <autumnwindji@foxmail.com>
2023-07-10 13:10:36 +0800

    新增了初始的读数据函数get_data()

--- src/data/Environment.py
+++ src/data/Environment.py
@@ -1,7 +1,10 @@
 from typing import List, Tuple
 
 import numpy as npy
-
+import json
+import os
+import csv
+from dateutil.parser import parse
 from data.Action import Action
 
 
@@ -49,3 +52,78 @@ class Environment:
             zero_paddings = [npy.zeros(shape=(self._n_project + self._n_worker,))] * (n - len(self._buffered_states))
             return zero_paddings + self._buffered_states
         return self._buffered_states[-n:]
+
+    def get_data(self) -> Tuple[dict, dict, dict, dict]:
+        p_path = os.path.abspath(os.path.dirname(__file__))
+        data_dir = p_path[:p_path.rindex('data')]+"dataset/"
+
+        ## read worker attribute: worker_quality
+        worker_quality = {}
+        csvfile = open(data_dir + "worker_quality.csv", "r")
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for line in csvreader:
+            if float(line[1]) > 0.0:
+                worker_quality[int(line[0])] = float(line[1]) / 100.0
+        csvfile.close()
+
+        ## read project id
+        file = open(data_dir + "project_list.csv", "r")
+        project_list_lines = file.readlines()
+        file.close()
+        project_dir = data_dir + "project/"
+        entry_dir = data_dir + "entry/"
+
+        all_begin_time = parse("2018-01-01T0:0:0Z")
+
+        project_info = {}
+        entry_info = {}
+        limit = 24
+        industry_list = {}
+        # print(project_list_lines)
+
+        for line in project_list_lines:
+            line = line.strip('\n').split(',')
+
+            project_id = int(line[0])
+            entry_count = int(line[1])
+            # print(project_dir + "project_" + str(project_id) + ".txt")
+            file = open(project_dir + "project_" + str(project_id) + ".txt", "r", errors='ignore')
+            htmlcode = file.read()
+            file.close()
+            text = json.loads(htmlcode)
+
+            project_info[project_id] = {}
+            project_info[project_id]["sub_category"] = int(text["sub_category"])  # project sub_category
+            project_info[project_id]["category"] = int(text["category"])  # project category
+            project_info[project_id]["entry_count"] = int(text["entry_count"])  # project answers in total
+            project_info[project_id]["start_date"] = parse(text["start_date"])  # project start date
+            project_info[project_id]["deadline"] = parse(text["deadline"])  # project end date
+
+            if text["industry"] not in industry_list:
+                industry_list[text["industry"]] = len(industry_list)
+            project_info[project_id]["industry"] = industry_list[text["industry"]]  # project domain
+
+            entry_info[project_id] = {}
+            k = 0
+            while (k < entry_count):
+                # print(entry_dir + "entry_" + str(project_id) + "_" + str(k) + ".txt", "r")
+                file = open(entry_dir + "entry_" + str(project_id) + "_" + str(k) + ".txt", "r", errors='ignore')
+                htmlcode = file.read()
+                file.close()
+                text = json.loads(htmlcode, strict=False)
+
+                for item in text["results"]:
+                    entry_number = int(item["entry_number"])
+                    entry_info[project_id][entry_number] = {}
+                    entry_info[project_id][entry_number]["entry_created_at"] = parse(
+                        item["entry_created_at"])  # worker answer_time
+                    entry_info[project_id][entry_number]["worker"] = int(item["entry_number"])  # work_id
+                k += limit
+
+        print("finish read_data")
+        return worker_quality, project_info, entry_info, industry_list
+
+if __name__ == "__main__":
+    environment = Environment(4, 3, [0,1,0,1], [1,0,0])
+    environment.get_data()
