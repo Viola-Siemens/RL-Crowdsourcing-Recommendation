11386bea4e79b0031814d445946e312a96cd51ad
gitCoder1024 <88184811+gitCoder1024@users.noreply.github.com>
2023-07-13 14:14:43 +0800

    朴素DQN

--- src/model/impl/ValueBased.py
+++ src/model/impl/ValueBased.py
@@ -1,24 +1,34 @@
+import random
 from typing import Callable
-
+import math
 import numpy as np
 import torch
-from torch import Tensor
+
 from torch import nn
 from torch.optim import Optimizer
+from torch import Tensor
 
-from data.Action import Action
 from data.Environment import Environment
 from model.FCNet import FCNet
 from model.ReinforcementAlgorithm import ReinforcementAlgorithm
+from data.Action import Action
+import torch.nn.functional as F
 
 n_gram = 5
 
 
 class QLearning(ReinforcementAlgorithm):
     def __init__(self, env: Environment):
-        policy_net = FCNet(env.get_state_dim(), [(env.get_output_dim(), None)],
-                           hidden_dims=[64, 256, 256])
+        policy_net = FCNet(env.get_state_dim() * n_gram, [(env.get_output_dim(), None)],
+                           hidden_dims=[256, 256])  # TODO
+        # target_net = FCNet(env.get_state_dim() * n_gram, [(env.get_output_dim(), None)],
+        #                    hidden_dims=[64, 128, 256, 256, 256])
+        # target_net.load_state_dict(policy_net.state_dict())
         super().__init__(policy_net)
+        # super().__init__(target_net)
+        # target_net.eval()
+        self.policy_net = policy_net
+        # self.target_net = target_net
         self.gamma = 0.5
         self.epsilon = 0.9
         self.epsilon_decay = 0.99
@@ -35,27 +45,34 @@ class QLearning(ReinforcementAlgorithm):
             it = 0
             while not env.is_done():
                 it += 1
-                state = env.get_state()
+                hist = env.get_history_states(n_gram - 1)
+                hist.append(env.get_state())
+                state = hist
                 state = Tensor(np.array(state).reshape(-1))
-                actions = self.net.forward(state)[0]
-                action = Action(int(torch.max(actions).item()))
+
+                # actions = self.policy_net.forward(state)[0]
+                # action = Action(int(random.choice(actions.tolist())))
+                action = env.sample()
                 reward = env.perform(action)
 
                 rewards.append(reward)
 
-                next_state = env.get_state()
+                hist = env.get_history_states(n_gram - 1)
+                hist.append(env.get_state())
+                next_state = hist
                 next_state = Tensor(np.array(next_state).reshape(-1))
-                next_actions = self.net.forward(next_state)[0]
+                next_actions = self.policy_net.forward(next_state)[0]
                 next_action = Action(int(torch.max(next_actions).item()))
 
-                target = reward + self.gamma * next_action.get()
-                loss_temp = ((target - action.get()) ** 2) / 2
+                target = reward + self.gamma * (next_action.get()/1807)
+                loss_temp = ((target - (action.get())/1807) ** 2) / 2
 
-                loss = loss + loss_temp
+                loss += loss_temp
 
-            loss = loss / it
+            loss /= it
             optimizer.zero_grad()
-            loss.backward() # TODO
+            loss = torch.tensor(loss, requires_grad=True)
+            loss.backward()
             optimizer.step()
             logger(e, float(np.mean(rewards)), float(loss))
 
