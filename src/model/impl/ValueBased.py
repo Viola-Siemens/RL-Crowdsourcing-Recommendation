123ab7cd0a8207f0a9ac7bc5cccc5b6153d957b3
gitCoder1024 <88184811+gitCoder1024@users.noreply.github.com>
2023-07-13 21:03:26 +0800

    修改value-based bug并加入ε-贪婪策略

--- src/model/impl/ValueBased.py
+++ src/model/impl/ValueBased.py
@@ -1,15 +1,18 @@
+
 from typing import Callable
 
 import numpy as np
 import torch
-from torch import Tensor
+
 from torch import nn
 from torch.optim import Optimizer
+from torch import Tensor
 
-from data.Action import Action
 from data.Environment import Environment
 from model.FCNet import FCNet
 from model.ReinforcementAlgorithm import ReinforcementAlgorithm
+from data.Action import Action
+import torch.nn.functional as F
 
 n_gram = 5
 
@@ -17,12 +20,12 @@ n_gram = 5
 class QLearning(ReinforcementAlgorithm):
     def __init__(self, env: Environment):
         policy_net = FCNet(env.get_state_dim() * n_gram, [(env.get_output_dim(), None)])
-
         super().__init__(policy_net)
+
         self.policy_net = policy_net
-        self.gamma = 0.5
-        self.epsilon = 0.9
-        self.epsilon_decay = 0.99
+        self.gamma = 0.9
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.995
         self.epsilon_min = 0.01
         self.loss_fn = nn.MSELoss()
         self.action_size = env.get_output_dim()
@@ -40,12 +43,14 @@ class QLearning(ReinforcementAlgorithm):
                 hist.append(env.get_state())
                 state = hist
                 state = Tensor(np.array(state).reshape(-1))
+                # ε-贪婪策略
+                if np.random.rand() <= self.epsilon:
+                    action=env.sample()
+                else:
+                    actions=self.policy_net.forward(state)[0]
+                    action = Action(torch.argmax(actions).item())
 
-                # actions = self.policy_net.forward(state)[0]
-                # action = Action(int(random.choice(actions.tolist())))
-                action = env.sample()
                 reward = env.perform(action)
-
                 rewards.append(reward)
 
                 hist = env.get_history_states(n_gram - 1)
@@ -53,7 +58,7 @@ class QLearning(ReinforcementAlgorithm):
                 next_state = hist
                 next_state = Tensor(np.array(next_state).reshape(-1))
                 next_actions = self.policy_net.forward(next_state)[0]
-                next_action = Action(int(torch.max(next_actions).item()))
+                next_action = Action(torch.argmax(next_actions).item())
 
                 target = reward + self.gamma * (next_action.get()/1807)
                 loss_temp = ((target - (action.get())/1807) ** 2) / 2
@@ -65,7 +70,8 @@ class QLearning(ReinforcementAlgorithm):
             loss = torch.tensor(loss, requires_grad=True)
             loss.backward()
             optimizer.step()
-            logger(e, float(np.mean(rewards)), 0)
+            logger(e, float(np.mean(rewards)), float(loss))
+            self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
 
     def get_algorithm_name(self) -> str:
         return "value-based"
